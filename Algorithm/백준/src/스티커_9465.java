4b1c92f3331b5551620815d9538d5762d9bb5708
jihong <15computer@hanmail.net>
2021-01-18 14:37:55 +0900

    test

--- Algorithm/백준/src/스티커_9465.java
+++ Algorithm/백준/src/스티커_9465.java
@@ -2,18 +2,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-// bfs
+// dp
 public class 스티커_9465 {
-	static class Node{
-		int r;
-		int c;
-		int sum;
-		Node(int r, int c, int sum){
-			this.r = r;
-			this.c = c;
-			this.sum = sum;
-		}
-	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
@@ -21,14 +12,24 @@ public class 스티커_9465 {
 		int[] dc = {1, -1, 1, -1};
 		for (int tc = 0; tc < T; tc++) {
 			int n = sc.nextInt();
-			int[][] sticky = new int[2][n];
+			int[][] sticky = new int[2][n+1];
 			
 			for (int i = 0; i < 2; i++) {
-				for (int j = 0; j < n; j++) {
+				for (int j = 1; j < n+1; j++) {
 					sticky[i][j] = sc.nextInt();
 				}
 			}
 			
+			int[][] dp = new int[2][n+1];
+			dp[0][1] = sticky[0][1];
+			dp[1][1] = sticky[1][1];
+			
+			for (int i = 2; i < n+1; i++) {
+				dp[0][i] = Math.max(dp[1][i-2], dp[1][i-1]) +sticky[0][i];
+				dp[1][i] = Math.max(dp[0][i-2], dp[0][i-1]) + sticky[1][i];
+			}
+			
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 		}
 	}
 }
