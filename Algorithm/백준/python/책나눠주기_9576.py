84e0c1ebe534a81fb0630e83efb8568800f143b3
JH <15computer@hanmail.net>
2021-02-12 20:44:20 +0900

    0212

--- Algorithm/백준/python/책나눠주기_9576.py
+++ Algorithm/백준/python/책나눠주기_9576.py
@@ -4,7 +4,25 @@ T = int(input())
 for _ in range(T):
     N, M = map(int,input().split())
 
-    book = []
+    books = []
     for _ in range(M):
         [a, b] = map(int, input().split())
-        book.append( [a, b] )
+        books.append( [a, b] )
+
+    # b를 기준으로 정렬 후, 같으면 a로 정렬
+    books.sort(key = lambda x: (x[1], x[0]))
+
+    # 정렬한 순서대로 a번의 책을 준다.
+
+    nl = [False for i in range(N+1)]
+
+    result = 0
+
+    for book in books:
+        for i in range(book[0], book[1]+1):
+            if nl[i] == False:
+                nl[i] = True
+                result += 1
+                break
+
+    print(result)
