e0f7d682abf0e79263babe4c27b014eb8a88aaed
jihong <15computer@hanmail.net>
2021-03-05 14:45:32 +0900

    til

---
+++ Algorithm/sw_expert/햄버거다이어트.java
@@ -0,0 +1,98 @@
+public void run() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    
+    int l = Integer.parseInt(br.readLine());
+    
+    for(int p = 0; p < l; p++) {
+        StringTokenizer tc = new StringTokenizer(br.readLine(), " ");
+        int number = Integer.parseInt(tc.nextToken());
+        limit = Integer.parseInt(tc.nextToken());
+        
+        int[] pList = new int[number];
+        int[] cList = new int[number];
+        
+        for(int i = 0; i < number; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int point = Integer.parseInt(st.nextToken());
+            int calorie = Integer.parseInt(st.nextToken());
+            
+            pList[i] = point;
+            cList[i] = calorie;
+        }
+        
+        
+        DFS(pList, cList, 0, pList[0], cList[0]);
+        sb.append("#").append(p+1).append(" ").append(max);
+        
+    }
+    
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+}
+
+public static void DFS(int[] pList, int[] cList, int idx, int point, int calorie) {
+    if(calorie > limit) return;
+    if(idx < pList.length) {
+        max = Math.max(point, max);
+        return;
+    }
+
+    
+    DFS(pList, cList, idx+1, point, calorie);
+    DFS(pList, cList, idx+1, point+pList[idx], calorie+cList[idx]);
+}
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int l = Integer.parseInt(br.readLine());
+        
+        for(int p = 0; p < l; p++) {
+            StringTokenizer tc = new StringTokenizer(br.readLine());
+            int number = Integer.parseInt(tc.nextToken());
+            limit = Integer.parseInt(tc.nextToken());
+            
+            int[] pList = new int[number];
+            int[] cList = new int[number];
+            
+            for(int i = 0; i < number; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int point = Integer.parseInt(st.nextToken());
+                int calorie = Integer.parseInt(st.nextToken());
+                
+                pList[i] = point;
+                cList[i] = calorie;
+            }
+            
+            
+            DFS(pList, cList, 0, pList[0], cList[0]);
+            sb.append("#").append(p+1).append(" ").append(max);
+            
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+	}
+    
+public static void DFS(int[] pList, int[] cList, int idx, int point, int calorie) {
+    if(calorie > limit) return;
+    if(idx == pList.length) {
+        max = Math.max(point, max);
+        return;
+    }
+
+    
+    DFS(pList, cList, idx+1, point, calorie);
+    DFS(pList, cList, idx+1, point+pList[idx], calorie+cList[idx]);
+}
+}
