2f41e277beb7c436c5bc0e822b95f6a8f7fc365e
jihong <15computer@hanmail.net>
2021-03-15 17:23:34 +0900

    aws

---
+++ Algorithm/이것이코딩테스트다/python/기둥과보설치.java
@@ -0,0 +1,19 @@
+package python;
+
+import java.util.Scanner;
+
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/60061
+ * @author 15com
+ *
+ */
+public class 기둥과보설치 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	}
+    public int[][] solution(int n, int[][] build_frame) {
+        int[][] answer = {};
+        return answer;
+    }
+}
